d8103d575ea1460b1c801c4bfbe4dce7449fbc4f
maxgodefroy <max@godefroy.net>
2026-02-10 18:07:27 +0100

    [Modules][Resources] Simplify `GenerateResourceTypeId` using `Murmur2Hash64`
    Replaced manual hash calculation logic with the streamlined `Murmur2Hash64` function from `Hashing` for improved readability and consistency.

--- Modules/Resources/Include/KryneEngine/Modules/Resources/ResourceTypeId.hpp
+++ Modules/Resources/Include/KryneEngine/Modules/Resources/ResourceTypeId.hpp
@@ -7,7 +7,7 @@
 #pragma once
 
 
-#include <KryneEngine/Core/Common/Types.hpp>
+#include <KryneEngine/Core/Math/Hashing.hpp>
 
 namespace KryneEngine::Modules::Resources
 {
@@ -15,25 +15,7 @@ namespace KryneEngine::Modules::Resources
 
     constexpr ResourceTypeId GenerateResourceTypeId(const char* _name, size_t _size)
     {
-        u64 value = 0;
-        switch (_size > 8 ? 8 : _size)
-        {
-        case 8: value |= static_cast<u64>(_name[7]) << 56;
-        case 7: value |= static_cast<u64>(_name[6]) << 48;
-        case 6: value |= static_cast<u64>(_name[5]) << 40;
-        case 5: value |= static_cast<u64>(_name[4]) << 32;
-        case 4: value |= static_cast<u64>(_name[3]) << 24;
-        case 3: value |= static_cast<u64>(_name[2]) << 16;
-        case 2: value |= static_cast<u64>(_name[1]) << 8;
-        case 1: value |= static_cast<u64>(_name[0]);
-        default: break;
-        }
-
-        // Based on Murmur2 hashing, see Math/Hashing.cpp
-        constexpr u64 kMurmurSeed = 0x9E37'79B9'7F4A'7C15ull;
-        constexpr u64 kMurmurPrime = 14'313'749'767'032'793'493ull;
-
-        return kMurmurSeed ^ (value * kMurmurPrime);
+        return Hashing::Murmur2::Murmur2Hash64(_name, _size);
     }
 
     template <size_t N>
