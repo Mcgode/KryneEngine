9a485fbff933caba82561c33c9e138d4e44ef0c5
maxgodefroy <max@godefroy.net>
2026-02-27 18:32:24 +0100

    [Modules][GuiLib] Add `ClayHelper` header with `ToClayString` utility function

---
+++ Modules/GuiLib/Include/KryneEngine/Modules/GuiLib/ClayHelper.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 27/02/2026.
+ */
+
+#pragma once
+
+#include <clay.h>
+
+namespace KryneEngine
+{
+    template <class String>
+    Clay_String ToClayString(const String& _string)
+    {
+        return Clay_String {
+            .isStaticallyAllocated = false,
+            .length = static_cast<int>(_string.size()),
+            .chars = _string.data(),
+        };
+    }
+}
