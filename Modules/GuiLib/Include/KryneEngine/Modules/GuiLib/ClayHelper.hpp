e9cf892e9ba68cccfa5b9f2f57e19a6b9408c973
maxgodefroy <max@godefroy.net>
2026-02-27 22:52:35 +0100

    [Modules][GuiLib] Add `ClayIsHovering` utility function to `ClayHelper` header

--- Modules/GuiLib/Include/KryneEngine/Modules/GuiLib/ClayHelper.hpp
+++ Modules/GuiLib/Include/KryneEngine/Modules/GuiLib/ClayHelper.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <clay.h>
+#include <KryneEngine/Core/Common/Utils/Macros.hpp>
 
 namespace KryneEngine
 {
@@ -18,5 +19,17 @@ namespace KryneEngine
             .length = static_cast<int>(_string.size()),
             .chars = _string.data(),
         };
+    }
+
+    KE_FORCEINLINE bool ClayIsHovering(const Clay_ElementId& _id, const float2& _mousePosition)
+    {
+        const Clay_ElementData elementData = Clay_GetElementData(_id);
+        const float2 topLeft { elementData.boundingBox.x, elementData.boundingBox.y };
+        const float2 bottomRight { elementData.boundingBox.x + elementData.boundingBox.width, elementData.boundingBox.y + elementData.boundingBox.height };
+
+        return _mousePosition.x >= topLeft.x
+            && _mousePosition.x <= bottomRight.x
+            && _mousePosition.y >= topLeft.y
+            && _mousePosition.y <= bottomRight.y;
     }
 }
