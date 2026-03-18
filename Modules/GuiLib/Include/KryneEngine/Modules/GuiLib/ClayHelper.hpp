e0860081fb812f757e9101cc4e55cd9a20d191a7
maxgodefroy <max@godefroy.net>
2026-03-18 20:48:33 +0100

    [Modules][GuiLib] Remove unused `ClayIsHovering` helper function
    Deleted the `ClayIsHovering` function from `ClayHelper.hpp` as it is no longer required.

--- Modules/GuiLib/Include/KryneEngine/Modules/GuiLib/ClayHelper.hpp
+++ Modules/GuiLib/Include/KryneEngine/Modules/GuiLib/ClayHelper.hpp
@@ -20,16 +20,4 @@ namespace KryneEngine
             .chars = _string.data(),
         };
     }
-
-    KE_FORCEINLINE bool ClayIsHovering(const Clay_ElementId& _id, const float2& _mousePosition)
-    {
-        const Clay_ElementData elementData = Clay_GetElementData(_id);
-        const float2 topLeft { elementData.boundingBox.x, elementData.boundingBox.y };
-        const float2 bottomRight { elementData.boundingBox.x + elementData.boundingBox.width, elementData.boundingBox.y + elementData.boundingBox.height };
-
-        return _mousePosition.x >= topLeft.x
-            && _mousePosition.x <= bottomRight.x
-            && _mousePosition.y >= topLeft.y
-            && _mousePosition.y <= bottomRight.y;
-    }
 }
