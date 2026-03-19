105b14d43d564000ce2247279ebcb222f9862e20
maxgodefroy <max@godefroy.net>
2026-03-19 11:36:25 +0100

    [Core][Platform] Move `OpaqueHandle` to `Helpers.hpp` and update includes
    Refactored `OpaqueHandle` to a new `Helpers.hpp` file for better organization. Updated `Platform.hpp` and build configuration to reference the new header.

---
+++ Core/Include/KryneEngine/Core/Platform/Helpers.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 19/03/2026.
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace KryneEngine::Platform
+{
+    /**
+     * @brief A utility handle struct that handles both an opaque pointer and an error state, packed into a single word
+     */
+    struct OpaqueHandle
+    {
+        enum class Error
+        {
+            Unknown = 0,
+            NameInUse,
+            NoHost,
+            InvalidPath,
+            AccessDenied,
+        };
+
+        void* m_handle = nullptr;
+
+        explicit OpaqueHandle(void* _handle): m_handle(_handle) {}
+        explicit OpaqueHandle(Error _error)
+        {
+            reinterpret_cast<uintptr_t&>(m_handle) = 1 | static_cast<uintptr_t>(_error) << 1;
+        }
+
+        [[nodiscard]] bool IsValid() const
+        {
+            return (reinterpret_cast<uintptr_t>(m_handle) & 1) == 0;
+        }
+
+        [[nodiscard]] Error GetError() const
+        {
+            return static_cast<Error>(reinterpret_cast<uintptr_t>(m_handle) >> 1);
+        }
+    };
+}
--- Core/Include/KryneEngine/Core/Platform/Platform.hpp
+++ Core/Include/KryneEngine/Core/Platform/Platform.hpp
@@ -11,40 +11,10 @@
 #include "KryneEngine/Core/Common/Types.hpp"
 #include "KryneEngine/Core/Math/Vector.hpp"
 #include "KryneEngine/Core/Memory/Allocators/Allocator.hpp"
+#include "KryneEngine/Core/Platform/Helpers.hpp"
 
 namespace KryneEngine::Platform
 {
-    /**
-     * @brief A utility handle struct that handles both an opaque pointer and an error state, packed into a single word
-     */
-    struct OpaqueHandle
-    {
-        enum class Error
-        {
-            Unknown = 0,
-            NameInUse,
-            NoHost,
-        };
-
-        void* m_handle = nullptr;
-
-        explicit OpaqueHandle(void* _handle): m_handle(_handle) {}
-        explicit OpaqueHandle(Error _error)
-        {
-            reinterpret_cast<uintptr_t&>(m_handle) = 1 | static_cast<uintptr_t>(_error) << 1;
-        }
-
-        [[nodiscard]] bool IsValid() const
-        {
-            return (reinterpret_cast<uintptr_t>(m_handle) & 1) == 0;
-        }
-
-        [[nodiscard]] Error GetError() const
-        {
-            return static_cast<Error>(reinterpret_cast<uintptr_t>(m_handle) >> 1);
-        }
-    };
-
     /**
      * @defgroup IPC methods
      * @{
