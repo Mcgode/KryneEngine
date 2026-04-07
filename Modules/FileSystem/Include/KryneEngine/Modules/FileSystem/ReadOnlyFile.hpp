f9bacdb5be69bd2d80068b758d042c08c2a4f576
maxgodefroy <max@godefroy.net>
2026-04-07 15:18:18 +0200

    [Modules][FileSystem] Add templated `ReadT` method to `ReadOnlyFile` for type-safe reads

--- Modules/FileSystem/Include/KryneEngine/Modules/FileSystem/ReadOnlyFile.hpp
+++ Modules/FileSystem/Include/KryneEngine/Modules/FileSystem/ReadOnlyFile.hpp
@@ -39,7 +39,13 @@ namespace KryneEngine::Modules::FileSystem
 
         [[nodiscard]] const FileFlags& GetFlags() const { return m_flags; }
 
-        [[nodiscard]] size_t Read(size_t _offset, eastl::span<std::byte> _buffer) const;
+        size_t Read(size_t _offset, eastl::span<std::byte> _buffer) const;
+
+        template <class T>
+        size_t ReadT(const size_t _offset, T* _ptr, const size_t _count = 1) const
+        {
+            return Read(_offset, { reinterpret_cast<std::byte*>(_ptr), _count * sizeof(T) });
+        }
 
     private:
         VirtualFileSystem* m_fileSystem = nullptr;
