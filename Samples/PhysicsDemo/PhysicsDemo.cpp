7495782d301cc459d6e5b7cc3d07bc5ec87ac2d5
maxgodefroy <max@godefroy.net>
2026-08-18 22:47:57 +0200

    [Samples] Add new PhysicsDemo sample with RenderGraph and ImGui integration

---
+++ Samples/PhysicsDemo/PhysicsDemo.cpp
@@ -0,0 +1,150 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 18/08/2026.
+ */
+
+#include "../RenderGraphDemo/Rendering/FullscreenPassCommon.hpp"
+#include "KryneEngine/Modules/RenderGraph/Builder.hpp"
+#include "KryneEngine/Modules/RenderGraph/Registry.hpp"
+
+
+#include <KryneEngine/Core/Profiling/TracyHeader.hpp>
+#include <KryneEngine/Core/Threads/FibersManager.hpp>
+#include <KryneEngine/Core/Window/Window.hpp>
+#include <KryneEngine/Modules/ImGui/Context.hpp>
+#include <KryneEngine/Modules/RenderGraph/RenderGraph.hpp>
+
+
+using namespace KryneEngine;
+using namespace KryneEngine::Modules;
+
+
+int main()
+{
+    TracySetProgramName("Physics demo");
+
+    KE_ZoneScoped("Physics demo");
+
+    AllocatorInstance allocator {};
+
+    FibersManager fibersManager(0, allocator);
+
+    GraphicsCommon::ApplicationInfo appInfo {};
+    appInfo.m_features.m_present = true;
+    appInfo.m_applicationName = "Physics demo - Kryne Engine";
+#if defined(KE_GRAPHICS_API_VK)
+    appInfo.m_api = GraphicsCommon::Api::Vulkan_1_0;
+    appInfo.m_applicationName += " - Vulkan";
+#elif defined(KE_GRAPHICS_API_DX12)
+    appInfo.m_api = GraphicsCommon::Api::DirectX12_1;
+    appInfo.m_applicationName += " - DirectX 12";
+#elif defined(KE_GRAPHICS_API_MTL)
+    appInfo.m_api = GraphicsCommon::Api::Metal_3;
+    appInfo.m_applicationName += " - Metal";
+#endif
+    Window mainWindow(appInfo, allocator);
+    GraphicsContext* graphicsContext = mainWindow.GetGraphicsContext();
+
+    Modules::ImGui::Context* imGuiContext = nullptr;
+
+    RenderGraph::RenderGraph renderGraph {};
+
+    DynamicArray<SimplePoolHandle> swapChainTextures(allocator, graphicsContext->GetFrameContextCount());
+    DynamicArray<SimplePoolHandle> swapChainRtvs(allocator, graphicsContext->GetFrameContextCount());
+
+    SimplePoolHandle imguiBufferDummy;
+
+    {
+        eastl::string nameTmp(allocator);
+
+        for (u32 i = 0; i < graphicsContext->GetFrameContextCount(); i++)
+        {
+            swapChainTextures[i] = renderGraph.GetRegistry().RegisterRawTexture(
+                graphicsContext->GetPresentTexture(i),
+                nameTmp.sprintf("Swap chain texture %d", i));
+
+            swapChainRtvs[i] = renderGraph.GetRegistry().RegisterRenderTargetView(
+                graphicsContext->GetPresentRenderTargetView(i),
+                swapChainTextures[i],
+                nameTmp.sprintf("Swap chain RTV %d", i));
+        }
+
+        imguiBufferDummy = renderGraph.GetRegistry().RegisterDummy("ImGui buffers");
+    }
+
+    do
+    {
+        if (imGuiContext == nullptr)
+        {
+            KE_ZoneScoped("Init ImGui context");
+
+            // Even if it's a dummy pass, the generated render pass should match signature with the one in the render
+            // graph for the ImGui pass, so it will be reused there.
+
+            RenderGraph::PassDeclaration imguiDummyPass(RenderGraph::PassType::Render, 0);
+            RenderGraph::PassDeclarationBuilder(imguiDummyPass, nullptr)
+                .SetName("ImGui pass")
+                .AddColorAttachment(swapChainRtvs[0])
+                    .SetLoadOperation(RenderPassDesc::Attachment::LoadOperation::Load)
+                    .SetStoreOperation(RenderPassDesc::Attachment::StoreOperation::Store)
+                    .Done();
+            imguiDummyPass.m_colorAttachments[0].m_layoutBefore = TextureLayout::ColorAttachment;
+            imguiDummyPass.m_colorAttachments[0].m_layoutAfter = TextureLayout::ColorAttachment;
+
+            imGuiContext = allocator.New<Modules::ImGui::Context>(
+                &mainWindow,
+                renderGraph.FetchRenderPass(*graphicsContext, imguiDummyPass),
+                allocator);
+        }
+
+        imGuiContext->NewFrame(&mainWindow);
+
+        RenderGraph::Builder& builder = renderGraph.BeginFrame(*graphicsContext);
+
+        SimplePoolHandle swapChainTexture = swapChainTextures[graphicsContext->GetCurrentPresentImageIndex()];
+        SimplePoolHandle swapChainRtv = swapChainRtvs[graphicsContext->GetCurrentPresentImageIndex()];
+
+        ::ImGui::ShowDemoWindow();
+
+        builder
+            .DeclarePass(RenderGraph::PassType::Transfer)
+                .SetName("Imgui transfer")
+                .SetExecuteFunction([imGuiContext](RenderGraph::RenderGraph&, const RenderGraph::PassExecutionData& _executionData)
+                {
+                    imGuiContext->PrepareToRenderFrame(_executionData.m_graphicsContext, _executionData.m_commandList);
+                })
+                .WriteDependency({
+                    .m_resource = imguiBufferDummy,
+                })
+                .Done()
+            .DeclarePass(RenderGraph::PassType::Render)
+                .SetName("ImGui pass")
+                .SetExecuteFunction([imGuiContext](RenderGraph::RenderGraph&, const RenderGraph::PassExecutionData& _executionData)
+                {
+                    imGuiContext->RenderFrame(_executionData.m_graphicsContext, _executionData.m_commandList);
+                })
+                .AddColorAttachment(swapChainRtv)
+                    .SetClearColor({ 1.f, 0.f, 1.f, 1.f })
+                    .SetLoadOperation(RenderPassDesc::Attachment::LoadOperation::Clear)
+                    .SetStoreOperation(RenderPassDesc::Attachment::StoreOperation::Store)
+                    .Done()
+                .ReadDependency({
+                    .m_resource = imguiBufferDummy,
+                })
+                .Done()
+            .DeclareTargetResource(swapChainTexture);
+
+        builder.BuildDag();
+        renderGraph.SubmitFrame(*graphicsContext, &fibersManager);
+    }
+    while (graphicsContext->EndFrame());
+
+    if (imGuiContext)
+    {
+        imGuiContext->Shutdown(&mainWindow);
+        allocator.Delete(imGuiContext);
+    }
+
+    return 0;
+}
