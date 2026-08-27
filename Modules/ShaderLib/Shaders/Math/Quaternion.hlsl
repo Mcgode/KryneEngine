3066c325fcad4014dfdbee743c50b95c9db27cad
maxgodefroy <max@godefroy.net>
2026-08-27 13:39:30 +0200

    [Modules][ShaderLib] Added Quaternion::Unpack64 function

--- Modules/ShaderLib/Shaders/Math/Quaternion.hlsl
+++ Modules/ShaderLib/Shaders/Math/Quaternion.hlsl
@@ -22,4 +22,30 @@ namespace Quaternion
     {
         return float4(-_quaternion.xyz, _quaternion.w);
     }
+
+    float4 Unpack64(const in uint2 _packed)
+    {
+        const uint highestIndex = bitfieldExtract(_packed.x, 2, 0);
+        const uint3 packedValues = uint3(
+            bitfieldExtract(_packed.x, 20, 2),
+            bitfieldExtract(_packed.x, 10, 22) | (bitfieldExtract(_packed.y, 10, 0) << 10),
+            bitfieldExtract(_packed.y, 20, 10)
+        );
+
+        const float scale = sqrt(2) / (float(1 << 19) -1.f);
+        const float3 lowestComponents = float3(packedValues) * scale;
+        const float highestComponent = sqrt(1 - dot(lowestComponents, lowestComponents));
+
+        float4 quaternion;
+        quaternion[highestIndex] = highestComponent;
+        uint offset = 0;
+        for (uint i = 0; i < 3; i++)
+        {
+            if (i == highestIndex)
+                offset++;
+            quaternion[i + offset] = lowestComponents[i];
+        }
+
+        return quaternion;
+    }
 }
