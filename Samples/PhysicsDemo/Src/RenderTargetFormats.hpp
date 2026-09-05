3de6eb487d0bf7bdc708d0b290381d7e4ada416f
maxgodefroy <max@godefroy.net>
2026-09-05 12:13:17 +0200

    [Samples][PhysicsDemo] Fixed texture format for GBuffer2

--- Samples/PhysicsDemo/Src/RenderTargetFormats.hpp
+++ Samples/PhysicsDemo/Src/RenderTargetFormats.hpp
@@ -14,7 +14,7 @@ namespace KryneEngine::Samples::PhysicsDemo
 {
     static constexpr auto kGBuffer0Format = TextureFormat::RGBA8_UNorm;
     static constexpr auto kGBuffer1Format = TextureFormat::RGBA8_UNorm;
-    static constexpr auto kGBuffer2Format = TextureFormat::RGB16_Float;
+    static constexpr auto kGBuffer2Format = TextureFormat::RGBA16_Float;
     static constexpr auto kGBufferDepthFormat = TextureFormat::D32F;
 
     static constexpr auto kShadowFormat = TextureFormat::D16;
